6facf735714e5a80445a8599ec11b59d6a61346a
Olivier Nguyen <nguyenolive@gmail.com>
2015-10-15 23:34:15 -0400

    added board display

--- display.py
+++ display.py
@@ -1,3 +1,45 @@
 """
 ASCII graphics for game
 """
+import string
+
+
+def print_board(board):
+    """
+    Prints the game board
+    """
+
+    print "The board look like this: \n"
+
+    #print the horizontal numbers
+    print " ",
+    for i in range(8):
+        #print "  " + str(i+1) + "  ",
+        print "  " + string.ascii_uppercase[i] + "  ",
+    print "\n"
+    s = "u"
+    for i in range(8):
+
+        #print the vertical line number
+        if i != 9:
+            print str(i+1) + "  ",
+        else:
+            print str(i+1) + " ",
+
+        #print the board values, and cell dividers
+        for j in range(8):
+            if board[i][j] == -1:
+                print ' ',
+            else:
+                print board[i][j],
+
+            if j != 8:
+                print " | ",
+        print str(i+1) + " ",
+        print
+
+        #print a horizontal line
+        if i != 8:
+            print "  -----------------------------------------------"
+        else:
+            print
--- game.py
+++ game.py
@@ -1,4 +1,15 @@
 """
 The logic behind how the Hungry Birds world works.
-This file describes several supporting types like AgentState, Agent, Direction, and Grid.
+TODO: Create board class?
 """
+
+def create_board():
+    N = 8;
+    board = []
+
+    for i in range(N):
+        board_row = []
+        for j in range(N):
+            board_row.append(' ')
+        board.append(board_row)
+    return board
