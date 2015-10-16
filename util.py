f765791963f74a7fe0d1292e4bdde2aa1d605729
Justin <justin_bunker@hotmail.com>
2015-10-16 10:34:51 -0400

    removed comment

--- util.py
+++ util.py
@@ -6,7 +6,6 @@ def CoorToIndex(coordinates):
     """For converting coordinate notation into array index."""
     rowSize = 8
     coordinates.lower()
-    print coordinates[0] + " " + str(ord(coordinates[0]))
     if not(97 <= ord(coordinates[0]) <= 104 \
     and 1 <= int(coordinates[1]) <= 8):
         raise Exception("Out of Bounds")
