7da07ec8b58858c11d5f4087560266750ba07d45
Justin <justin_bunker@hotmail.com>
2015-10-16 21:32:25 -0400

    recoded index conversion function

--- util.py
+++ util.py
@@ -2,13 +2,25 @@
 Useful data structures for implementing search algorithms.
 """
 
-def CoorToIndex(coordinates):
+import game
+
+# def CoorToIndex(coordinates):
+#     """For converting coordinate notation into array index."""
+#     rowSize = 8
+#     coordinates.lower()
+#     if not(97 <= ord(coordinates[0]) <= 104 \
+#     and 1 <= int(coordinates[1]) <= 8):
+#         raise Exception("Out of Bounds")
+#     rankNumber = int(coordinates[1]) - 1
+#     fileLetterValue = ord(coordinates[0]) - 97
+#     return rowSize * rankNumber + fileLetterValue
+
+def ConvertToIndex(stringCoodinates):
     """For converting coordinate notation into array index."""
-    rowSize = 8
-    coordinates.lower()
-    if not(97 <= ord(coordinates[0]) <= 104 \
-    and 1 <= int(coordinates[1]) <= 8):
+    stringCoodinates.lower()
+    if not(97 <= ord(stringCoodinates[0]) <= 104 \
+    and 1 <= int(stringCoodinates[1]) <= 8):
         raise Exception("Out of Bounds")
-    rankNumber = int(coordinates[1]) - 1
-    fileLetterValue = ord(coordinates[0]) - 97
-    return rowSize * rankNumber + fileLetterValue
+    x = ord(stringCoodinates[0]) - 97
+    y = 7 - (int(stringCoodinates[1]) - 1)
+    return game.Coordinates(x, y)
