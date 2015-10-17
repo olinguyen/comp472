7115a8b7da7078aea7b8ec103982917e706310e7
Justin <justin_bunker@hotmail.com>
2015-10-16 21:41:16 -0400

    removed comments

--- util.py
+++ util.py
@@ -4,17 +4,6 @@ Useful data structures for implementing search algorithms.
 
 import game
 
-# def CoorToIndex(coordinates):
-#     """For converting coordinate notation into array index."""
-#     rowSize = 8
-#     coordinates.lower()
-#     if not(97 <= ord(coordinates[0]) <= 104 \
-#     and 1 <= int(coordinates[1]) <= 8):
-#         raise Exception("Out of Bounds")
-#     rankNumber = int(coordinates[1]) - 1
-#     fileLetterValue = ord(coordinates[0]) - 97
-#     return rowSize * rankNumber + fileLetterValue
-
 def ConvertToIndex(stringCoodinates):
     """For converting coordinate notation into array index."""
     stringCoodinates.lower()
