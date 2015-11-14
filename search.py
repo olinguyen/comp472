e5fabff12e3dbdf34c274d8816c5a18063c10a41
Justin <justin_bunker@hotmail.com>
2015-11-14 16:17:23 -0500

    minimax search

--- search.py
+++ search.py
@@ -1,3 +1,6 @@
+from board import Board
+from copy import copy
+
 """
 All search algorithms.
 """
@@ -5,21 +8,22 @@ MAXDEPTH = 2
 
 class Node:
     numberOfNodes = 0
-    def __init__(self, parent, isMax, depth):
+    def __init__(self, board, parent, isMax, depth):
         self.score = None
         self.parent = parent
         self.children = []
         self.depth = depth
         self.isMax = isMax
-        Node.numberOfNodes += 1
-        print 'Depth with ' + str(self.depth) + ' created'
+        self.board = board
         self.score = Node.numberOfNodes
-    def generateChildren(self):
-        self.children.append(Node(self, not self.isMax, self.depth + 1))
-        self.children.append(Node(self, not self.isMax, self.depth + 1))
+        Node.numberOfNodes += 1
+        # print 'Depth with ' + str(self.depth) + ' created'
+    def generateChildren(self, listOfValidMoves):
+        for move in self.board.getPossibleMoves():
+            board = Board()
+            self.children.append(Node(board, self, not self.isMax, self.depth+1))
     def evaluateScore(self):
-        #self.score = Node.numberOfNodes
-        return
+        self.score = board.getBasicHeuristic()
     def returnMinChildScore(self):
         mini = self.children[0].score
         for child in self.children:
@@ -32,14 +36,26 @@ class Node:
             if child.score > maxi:
                 maxi = child.score
         return maxi
+        
+    def getBestMove(self):
+        for child in self.children:
+            if child.score == self.score:
+                for index, agent in enumerate(self.board.agents):
+                    if self.board.agents[index].x != child.agents[index].x:
+                        src_coordinates = self.board.agents[index].coordinates
+                        dst_coordinates = child.agents[index].coordinates
+                        break
+
+        return src_coordinates, dst_coordinates
+
 
 def MiniMax(node):
 	"""
 	Implementation of the minimax algorithm
 	"""
-    print '\nGenerating children...'
+    # print '\nGenerating children...'
     node.generateChildren()
-    print node.children[0].depth
+    # print node.children[0].depth
 
     if node.depth != MAXDEPTH - 1:
         for child in node.children:
@@ -47,14 +63,12 @@ def MiniMax(node):
     else:
         for child in node.children:
             child.evaluateScore()
-            print 'Child score is: ' + str(child.score)
+            # print 'Child score is: ' + str(child.score)
 
     if node.children[0].score:
         if node.isMax:
             node.score = node.returnMaxChildScore()
-            print 'MaxChildScore is ' + str(node.score)
+            # print 'MaxChildScore is ' + str(node.score)
         else:
             node.score = node.returnMinChildScore()
-            print 'MinChildScore is ' + str(node.score)
-
-    return
+            # print 'MinChildScore is ' + str(node.score)
